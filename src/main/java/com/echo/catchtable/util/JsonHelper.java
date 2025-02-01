eb4c592b19af6ab2ef3f0d2b91989022d4aeeb31
itaebro <plus100kt@gmail.com>
2025-02-02 02:22:54 +0900

    feat(util): json helper

---
+++ src/main/java/com/echo/catchtable/util/JsonHelper.java
@@ -0,0 +1,20 @@
+package com.echo.catchtable.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Arrays;
+
+public class JsonHelper extends ObjectMapper {
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    public String jsonArrayToString(Object[] jsonList) {
+        return Arrays.stream(jsonList).map(weekInfo -> {
+            try {
+                return objectMapper.writeValueAsString(weekInfo);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }).toList().toString();
+    }
+}
